929ef80b77741ded0636b7f92c99bc0b21e3b22c
Benedict Afotey <palomakut1@gmail.com>
2025-12-12 14:32:25 +0000

    feat:implemnet the contibutions graph

--- src/modules/contributions/contributions.module.ts
+++ src/modules/contributions/contributions.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ContributionsService } from './contributions.service';
 import { ContributionsController } from './contributions.controller';
 import { PrismaService } from '../../prisma/prisma.service';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-    imports: [],
+    imports: [AuthModule],
     controllers: [ContributionsController],
     providers: [ContributionsService, PrismaService],
 })
