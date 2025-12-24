560f1e6cb933b1474b19ec74055c61d9fa9462f2
Benedict Afotey <palomakut1@gmail.com>
2025-12-24 18:50:07 +0000

    feat: Implement user statistics service and admin API for global user data.

--- src/modules/admin/admin.module.ts
+++ src/modules/admin/admin.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AdminQuizzesController } from './controllers/admin.quizzes.controller';
+import { AdminStatsController } from './controllers/admin.stats.controller';
 import { QuizzesModule } from '../quizzes/quizzes.module';
+import { StatsModule } from '../stats/stats.module';
 
 
 @Module({
-  imports: [QuizzesModule],
-  controllers: [AdminQuizzesController],
+  imports: [QuizzesModule, StatsModule],
+  controllers: [AdminQuizzesController, AdminStatsController],
 })
 export class AdminModule { }
